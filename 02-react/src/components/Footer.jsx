3741fb817c9a78b4d3a8768635a9e2e222739a94
Roman <romainot99@gmail.com>
2025-11-20 16:49:02 +0100

    Custom header and footer components

---
+++ 02-react/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+      return(
+         <footer style={{ "paddingBottom": "2000px"}}>
+        <small>&copy; 2025 DevJobs. Todos los derechos reservados.</small>
+      </footer>
+      )
+}
+export default Footer;
