53b41ed5660f1c2ec55cccc84769ae94eac68036
Roman <romainot99@gmail.com>
2025-11-24 17:36:04 +0100

    Implement pagination for job listings
    Refactored App.jsx to paginate job listings using a constant for results per page and dynamic calculation of total pages. JobListings now receives only the jobs for the current page as a prop, improving performance and enabling pagination.

--- 02-react/src/App.jsx
+++ 02-react/src/App.jsx
@@ -4,12 +4,20 @@ import {Header} from "./components/Header.jsx";
 import {Pagination} from "./components/Pagination.jsx";
 import {SearchFormSection} from "./components/SearchFormSection.jsx";
 import {JobListings} from "./components/JobListings.jsx";
+import jobsData from "./data.json";
+
+const RESULTS_PER_PAGE = 5
 
 function App() {
 
   const [currentPage, setCurrentPage] = useState(1)
-  const totalPages = 5
+  const totalPages = Math.ceil(jobsData.length / RESULTS_PER_PAGE)
 
+
+  const pageResults = jobsData.slice(
+    (currentPage-1) * RESULTS_PER_PAGE, //Pagina 1 -> 0, Pagina 2 -> 5, Pagina 3 -> 10
+    currentPage * RESULTS_PER_PAGE //Pagina 1 -> 5, Pagina 2 -> 10, Pagina 3 -> 15
+  )
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
@@ -22,7 +30,7 @@ function App() {
       <main>
         <SearchFormSection />
         <section>
-          <JobListings />
+          <JobListings jobs={pageResults} />
           <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
         </section>
       </main>
--- 02-react/src/components/JobListings.jsx
+++ 02-react/src/components/JobListings.jsx
@@ -1,13 +1,11 @@
-import jobsData from "../data.json";
 import { JobCard } from "./JobCard.jsx";
 
-
-export function JobListings() {
+export function JobListings({jobs}) {
   return (
     <>
       <h2>Resultados de búsqueda</h2>
       <div className="jobs-listings">
-        {jobsData.map((job) => (
+        {jobs.map((job) => (
           <JobCard key={job.id} job={job} />
         ))}
       </div>
