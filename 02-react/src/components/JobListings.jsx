d9246646d1df0570d6db6f1519d3c0095304d06e
Roman <romainot99@gmail.com>
2025-11-24 16:20:36 +0100

    Componetizacion

---
+++ 02-react/src/components/JobListings.jsx
@@ -0,0 +1,24 @@
+import data from "../data.json";
+
+
+function JobListings() {
+  return (
+    <>
+      <h2>Resultados de búsqueda</h2>
+      <div className="jobs-listings">
+        {data.map((empleo) => (
+          <div key={empleo.id}>
+            <h3>{empleo.titulo}</h3>
+            <small>
+              {empleo.empresa} | {empleo.ubicacion}
+            </small>
+            <p>{empleo.descripcion}</p>
+          </div>
+        ))}
+      </div>
+      <button className="button-apply-job">Aplicar</button>
+    </>
+  );
+}
+
+export default JobListings;
